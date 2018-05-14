c94f2d40107b444149a17edff128920cac269671
佐々木健佑 <sasakikensuke@13351126211.ap.cc.tsukuba.ac.jp>
2018-05-14 15:25:19 +0900

    pass test code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,8 +2,12 @@
 
 def fizzbuzz(num):
 
-    if num % 3 == 0 and num % 5 == 0:
+    if num % 15 == 0:
         return 'FizzBuzz'
+    elif num % 3 == 0:
+        return 'Fizz'
+    elif num % 5 == 0:
+        return 'Buzz'
     else:
         return str(num)
 
